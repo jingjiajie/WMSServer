e0a01f5110bb028d536292806d6ab79a4a715364
jingjiajie <932166095@qq.com>
2018-03-27 20:35:14 +0800

    tmp

--- LedgerService/src/main/java/com/wms/services/ledger/LedgerService.java
+++ LedgerService/src/main/java/com/wms/services/ledger/LedgerService.java
@@ -1,10 +1,14 @@
 package com.wms.services.ledger;
 
+import com.wms.services.ledger.model.Person;
+import com.wms.services.ledger.service.PersonService;
+import com.wms.services.ledger.service.PersonServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
@@ -13,7 +17,15 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource({"classpath:applicationContext.xml"})
 public class LedgerService {
     public static void main(String args[]){
-        SpringApplication.run(LedgerService.class,args);
+        ApplicationContext applicationContext = SpringApplication.run(LedgerService.class,args);
         System.out.println("总账服务启动...");
+        Person person = new Person();
+        person.setName("小黑");
+        person.setPassword("987654321");
+        person.setRole("管理员");
+        person.setAuthorityString("HEHE");
+        PersonService personService = applicationContext.getBean(PersonService.class);
+        personService.add("WMS_Template",new Person[]{person});
+        System.out.println("添加完成！");
     }
 }
