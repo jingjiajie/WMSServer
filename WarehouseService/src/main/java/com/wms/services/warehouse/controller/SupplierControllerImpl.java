6424188287913c0d84a25ad7ff36168f64e64a25
hsjjaaaa <a098100@163.com>
2018-03-30 12:13:50 +0800

    123

--- WarehouseService/src/main/java/com/wms/services/warehouse/controller/SupplierControllerImpl.java
+++ WarehouseService/src/main/java/com/wms/services/warehouse/controller/SupplierControllerImpl.java
@@ -1,4 +1,7 @@
 package com.wms.services.warehouse.controller;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.wms.services.warehouse.model.Supplier;
 
 public class SupplierControllerImpl implements SupplierController {
 }
