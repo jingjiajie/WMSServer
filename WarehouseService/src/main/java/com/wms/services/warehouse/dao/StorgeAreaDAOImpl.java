d8243f68486b02c33e3883131aadc924b519a7cd
a098100 <a098100@163.com>
2018-04-01 01:09:36 +0800

    123

---
+++ WarehouseService/src/main/java/com/wms/services/warehouse/dao/StorgeAreaDAOImpl.java
@@ -0,0 +1,104 @@
+package com.wms.services.warehouse.dao;
+
+import com.wms.services.warehouse.model.StorageArea;
+import com.wms.services.warehouse.model.Supplier;
+import com.wms.utilities.datastructures.Condition;
+import com.wms.utilities.exceptions.dao.DatabaseNotFoundException;
+import com.wms.utilities.exceptions.dao.WMSDAOException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@Repository
+public class StorgeAreaDAOImpl implements StorgeAreaDAO {
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+    private SessionFactory sessionFactory;
+    public int[] add(String database,StorageArea[] storageAreas) throws WMSDAOException {
+        if(storageAreas.length == 0){
+            return new int[0];
+        }
+        if(sessionFactory==null){
+            System.out.println("sessionFactory 为空");
+        }
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.createNativeQuery("USE " + database + ";").executeUpdate();
+        }catch (Throwable ex){
+            throw new DatabaseNotFoundException(database);
+        }
+        try {
+            for (StorageArea storageArea : storageAreas) {
+                session.save(storageArea);
+            }
+            int ids[] = Stream.of(storageAreas).mapToInt((p) -> p.getId()).toArray();
+            return ids;
+        }catch (Throwable ex){
+            throw new WMSDAOException(ex.getMessage());
+        }
+    }
+    public void update(String database,StorageArea storageAreas[]) throws WMSDAOException{
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.createNativeQuery("USE " + database + ";").executeUpdate();
+        }catch (Throwable ex){
+            throw new DatabaseNotFoundException(database);
+        }
+        try {
+            for (StorageArea storageArea : storageAreas) {
+                StringBuffer sbHQLString = new StringBuffer();
+                session.update(storageArea);
+            }
+        }catch (Throwable ex){
+            throw new WMSDAOException(ex.getMessage());
+        }
+    }
+
+    public void remove(String database, int ids[]) throws WMSDAOException{
+        if(ids.length == 0){
+            return;
+        }
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.createNativeQuery("USE " + database + ";").executeUpdate();
+        }catch (Throwable ex){
+            throw new DatabaseNotFoundException(database);
+        }
+        try {
+            StringBuffer idStr = new StringBuffer();
+            for (int id : ids) {
+                idStr.append(String.format("%d,", id));
+            }
+            idStr.setLength(idStr.length() - 1);
+            session.createQuery(String.format("delete from StorageArea where ID in(%s)", idStr.toString())).executeUpdate();
+        }catch (Throwable ex){
+            throw new WMSDAOException(ex.getMessage());
+        }
+    }
+    public StorageArea[] find(String database,Condition cond) throws WMSDAOException{
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.createNativeQuery("USE " + database + ";").executeUpdate();
+        }catch (Throwable ex){
+            throw new DatabaseNotFoundException(database);
+        }
+        try {
+            Query query = cond.createQuery("Storage", session);
+            List<StorageArea> listStorageArea = query.list();
+            StorageArea[] arrStorageArea = new StorageArea[listStorageArea.size()];
+            listStorageArea.toArray(arrStorageArea);
+            return arrStorageArea;}
+        catch (Throwable ex){
+            throw new WMSDAOException(ex.getMessage());
+        }
+    }
+
+}
---
+++ WarehouseService/src/main/java/com/wms/services/warehouse/model/StorageArea.java
@@ -0,0 +1,63 @@
+package com.wms.services.warehouse.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "StorageArea", schema = "WMS_Template", catalog = "")
+public class StorageArea {
+    private int id;
+    private String no;
+    private String name;
+
+    @Id
+    @Column(name = "ID")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "No")
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    @Basic
+    @Column(name = "Name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        StorageArea that = (StorageArea) o;
+
+        if (id != that.id) return false;
+        if (no != null ? !no.equals(that.no) : that.no != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (no != null ? no.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
