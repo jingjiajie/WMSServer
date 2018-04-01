9154668fd3547aa149830e2fb01483537ac49a3c
a098100 <a098100@163.com>
2018-04-01 22:59:08 +0800

    123

--- WarehouseService/src/main/java/com/wms/services/warehouse/service/SupplierServicesImpl.java
+++ WarehouseService/src/main/java/com/wms/services/warehouse/service/SupplierServicesImpl.java
@@ -2,6 +2,7 @@ package com.wms.services.warehouse.service;
 
 import com.wms.services.warehouse.dao.SupplierDAO;
 import com.wms.services.warehouse.model.Supplier;
+import com.wms.services.warehouse.model.Supply;
 import com.wms.utilities.datastructures.Condition;
 import com.wms.utilities.exceptions.dao.DatabaseNotFoundException;
 import com.wms.utilities.exceptions.service.WMSServiceException;
@@ -12,24 +13,69 @@ import org.springframework.transaction.annotation.Transactional;
 public class SupplierServicesImpl implements SupplierServices{
     @Autowired
     SupplierDAO  supplierDAO;
+
     @Transactional
-    public int[] add(String accountBook, Supplier[] suppliers) throws WMSServiceException{
+    public int[] add(String accountBook, Supplier[] suppliers) throws WMSServiceException
+    {
+        for (int i=0;i<suppliers.length;i++)
+        {
+        Supplier[] suppliersRepet;
+        String supplierName = suppliers[i].getName();
+        Condition condition = Condition.fromJson("{\"conditions\":[{\"key\":\"name\",\"values\":[\"" + supplierName + "\"],\"relation\":\"EQUAL\"}], \"orders\":[{\"key\":\"name\",\"order\":\"ASC\"}]}");
         try{
-            return supplierDAO.add(accountBook,suppliers);
+            suppliersRepet = supplierDAO.find(accountBook,condition);
         }catch (DatabaseNotFoundException ex){
             throw new WMSServiceException("Accountbook "+accountBook+" not found!");
         }
+        if(suppliersRepet.length>0)
+        {
+            break;
+        }
+        }
+        try
+        { return supplierDAO.add(accountBook,suppliers);
+        }catch (DatabaseNotFoundException ex){
+            throw new WMSServiceException("Accountbook "+accountBook+" not found!");
+        }
+
     }
+
     @Transactional
     public void update(String accountBook, Supplier[] suppliers) throws WMSServiceException{
+        for (int i=0;i<suppliers.length;i++)
+        {
+            Supplier[] suppliersRepet;
+            String supplierName = suppliers[i].getName();
+            Condition condition = Condition.fromJson("{\"conditions\":[{\"key\":\"name\",\"values\":[\"" + supplierName + "\"],\"relation\":\"EQUAL\"}], \"orders\":[{\"key\":\"name\",\"order\":\"ASC\"}]}");
+            try{
+                suppliersRepet = supplierDAO.find(accountBook,condition);
+            }catch (DatabaseNotFoundException ex){
+                throw new WMSServiceException("Accountbook "+accountBook+" not found!");
+            }
+            if(suppliersRepet.length>0)
+            {return;
+             }
+        }
         try {
             supplierDAO.update(accountBook, suppliers);
         }catch (DatabaseNotFoundException ex){
             throw new WMSServiceException("Accountbook "+accountBook+" not found!");
         }
     }
+
     @Transactional
     public void remove(String accountBook, int[] ids) throws WMSServiceException{
+
+        for (int i=0;i<ids.length;i++)
+        {
+            Supply[] supplies;
+            Supplier supplierRefference;
+            int SupplierID=ids[i];
+            Condition condition = Condition.fromJson("{\"conditions\":[{\"key\":\"SupplierID\",\"values\":[\"" + SupplierID + "\"],\"relation\":\"EQUAL\"}], \"orders\":[{\"key\":\"name\",\"order\":\"ASC\"}]}");
+            //用SupplyDAO
+        }
+
+
         try {
             supplierDAO.remove(accountBook, ids);
         } catch (DatabaseNotFoundException ex) {
@@ -44,5 +90,4 @@ public class SupplierServicesImpl implements SupplierServices{
             throw new WMSServiceException("Accountbook "+accountBook+" not found!");
         }
     }
-
 }
