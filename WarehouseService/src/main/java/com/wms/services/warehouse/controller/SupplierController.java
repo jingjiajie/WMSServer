3f070811a000f4ef6387fd0b2ffa11b9d61b270c
a098100 <a098100@163.com>
2018-04-01 15:09:54 +0800

    123

--- WarehouseService/src/main/java/com/wms/services/warehouse/controller/SupplierController.java
+++ WarehouseService/src/main/java/com/wms/services/warehouse/controller/SupplierController.java
@@ -1,9 +1,10 @@
 package com.wms.services.warehouse.controller;
+import com.wms.services.warehouse.model.StorageArea;
 import com.wms.services.warehouse.model.Supplier;
 import org.springframework.http.ResponseEntity;
 public interface SupplierController {
     ResponseEntity<int[]> add(String accountBook, Supplier[] suppliers);
     void update(String accountBook,Supplier suppliers[]);
     void remove(String accountBook,String strIDs);
-    ResponseEntity<Supplier[]> find(String accountBook,String condStr);
+    ResponseEntity<Supplier[]> find(String accountBook, String condStr);
 }
--- WarehouseService/src/main/java/com/wms/services/warehouse/service/SupplierServicesImpl.java
+++ WarehouseService/src/main/java/com/wms/services/warehouse/service/SupplierServicesImpl.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 @Service
-public class SipplierServicesImpl  implements SupplierServices{
+public class SupplierServicesImpl implements SupplierServices{
     @Autowired
     SupplierDAO  supplierDAO;
     @Transactional
