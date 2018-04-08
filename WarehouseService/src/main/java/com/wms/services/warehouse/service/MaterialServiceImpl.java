9848cd8df267c1e4cec6487939ad324460c3039a
陈光晃 <wingmonth@outlook.com>
2018-04-08 19:37:26 +0800

    some

--- WarehouseService/src/main/java/com/wms/services/warehouse/service/MaterialServiceImpl.java
+++ WarehouseService/src/main/java/com/wms/services/warehouse/service/MaterialServiceImpl.java
@@ -1,8 +1,8 @@
 package com.wms.services.warehouse.service;
 
 import com.wms.services.warehouse.dao.MaterialDAO;
-import com.wms.services.warehouse.dao.SupplyDAO;
 import com.wms.services.warehouse.model.Material;
+import com.wms.services.warehouse.dao.SupplyDAO;
 import com.wms.services.warehouse.model.Supply;
 import com.wms.utilities.datastructures.Condition;
 import com.wms.utilities.exceptions.dao.DatabaseNotFoundException;
@@ -15,10 +15,39 @@ import org.springframework.transaction.annotation.Transactional;
 public class MaterialServiceImpl implements MaterialService {
     @Autowired
     MaterialDAO materialDAO;
+    @Autowired
+    SupplyService supplyService;
     SupplyDAO  supplyDAO;
 
     @Transactional
     public int[] add(String accountBook, Material[] materials) throws WMSServiceException{
+        for (int i=0;i<materials.length;i++) {
+            //判断物料是否为空
+            String materialName = materials[i].getName();;//获取供货物料名称
+            if (materialName == null || materialName.trim().length() <=0) {       //判断是否为空，trim()去除字符串两边空格
+                throw new WMSServiceException("物料名称不能为空！");
+            }
+            String materialNo = materials[i].getNo();//获取供货物料代号
+            if (materialNo == null || materialNo.trim().length() <=0) {       //判断是否为空，trim()去除字符串两边空格
+                throw new WMSServiceException("物料代号不能为空！");
+            }
+            /*
+            Supply[] materialsRepeat=null;//新建一个数组，物料复述
+            Condition condition = Condition.fromJson("{'conditions':[{'key':'Name','values':['"+materialName+"'],'relation':'EQUAL'}],'orders':[{'key':'Name','order':'ASC'}]}");
+            //添加供货商-物料关联查询条件，按供货商ID排序
+            try {
+                materialsRepeat = supplyDAO.find(accountBook, condition);
+            } catch (DatabaseNotFoundException ex) {
+                throw new WMSServiceException("Accountbook " + accountBook + " not found!");
+            }
+            if (materialsRepeat.length > 0) {
+                throw new WMSServiceException("物料名称 " + materialName + " 已经存在！");
+            }
+            //判断物料是否唯一
+            */
+            materials[i].setWarehouseId(1);//先添加一个仓库ID，后面修正
+        }
+
         try{
             return materialDAO.add(accountBook,materials);
         }catch (DatabaseNotFoundException ex){
@@ -28,6 +57,36 @@ public class MaterialServiceImpl implements MaterialService {
 
     @Transactional
     public void update(String accountBook, Material[] materials) throws WMSServiceException{
+
+        for (int i=0;i<materials.length;i++) {
+            //判断物料是否为空
+            String materialName = materials[i].getName();;//获取供货物料名称
+            if (materialName == null || materialName.trim().length() <=0) {       //判断是否为空，trim()去除字符串两边空格
+                throw new WMSServiceException("物料名称不能为空！");
+            }
+            String materialNo = materials[i].getNo();//获取供货物料代号
+            if (materialNo == null || materialNo.trim().length() <=0) {       //判断是否为空，trim()去除字符串两边空格
+                throw new WMSServiceException("物料代号不能为空！");
+            }
+
+            Supply[] materialsRepeat=null;//新建一个数组，物料复述
+            Condition condition = Condition.fromJson("{'conditions':[{'key':'Name','values':['"+materialName+"'],'relation':'EQUAL'}],'orders':[{'key':'Name','order':'ASC'}]}");
+            //添加查询条件
+            try {
+                materialsRepeat = supplyDAO.find(accountBook, condition);
+            } catch (DatabaseNotFoundException ex) {
+                throw new WMSServiceException("Accountbook " + accountBook + " not found!");
+            }
+            if (materialsRepeat.length > 0) {
+                throw new WMSServiceException("物料名称 " + materialName + " 已经存在！");
+            }
+            //判断物料-供货商组合是否唯一
+        }
+        for (int i=0;i<materials.length;i++)
+        {
+            materials[i].setWarehouseId(1);//先添加一个仓库ID，后面修正
+        }
+
         try {
             materialDAO.update(accountBook, materials);
         }catch (DatabaseNotFoundException ex){
@@ -38,25 +97,22 @@ public class MaterialServiceImpl implements MaterialService {
     @Transactional
     public void remove(String accountBook, int[] ids) throws WMSServiceException{
 
-        for (int i=0;i<ids.length;i++)
+        //被供货引用无法删除，后需改为停用服务，需要请求消息窗口
+        Supply[] supplies;
+        int idLength=ids.length;
+        for (int i=0;i<idLength;i++)
         {
-
-            Supply[] supplies;
-            Supply[] materialRefference;
-            int MaaterialID=ids[i];
-            Condition condition = Condition.fromJson("{\"conditions\":[{\"key\":\"MaaterialID\",\"values\":[\"" + MaaterialID + "\"],\"relation\":\"EQUAL\"}], \"orders\":[{\"key\":\"name\",\"order\":\"ASC\"}]}");
-
-            try{
-                materialRefference = supplyDAO.find(accountBook,condition);
-            }catch (DatabaseNotFoundException ex){
-                throw new WMSServiceException("Accountbook "+accountBook+" not found!");
+            Material[] materialRefference=null;
+            int MaterialID=ids[i];
+            Condition condition = Condition.fromJson("{'conditions':[{'key':'materialID','values':['" + MaterialID + "'],'relation':'EQUAL'}]}");
+            supplies =supplyService.find(accountBook,condition);
+            if(supplies.length>0){
+                Condition conditionMaterial= Condition.fromJson("{'conditions':[{'key':'id','values':['" + MaterialID + "'],'relation':'EQUAL'}]}");
+                materialRefference=materialDAO.find("WMS_Template",conditionMaterial);
+                throw new WMSServiceException("物料 " +materialRefference[0].getName() + " 被引用无法删除!");
             }
-            if(materialRefference.length>0)
-            {
-                break;
-            }
-            //如果被供货信息引用则无法删除
         }
+
 
         try {
             materialDAO.remove(accountBook, ids);
