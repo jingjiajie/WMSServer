47a1b6d3343d4f6151534afda185692970164323
陈光晃 <wingmonth@outlook.com>
2018-04-26 20:46:31 +0800

    some

--- Utilities/src/main/java/com/wms/utilities/vaildator/Validator.java
+++ Utilities/src/main/java/com/wms/utilities/vaildator/Validator.java
@@ -1,5 +1,6 @@
 package com.wms.utilities.vaildator;
 import com.sun.xml.internal.ws.api.message.ExceptionHasMessage;
+import com.thoughtworks.xstream.mapper.Mapper;
 import com.wms.utilities.exceptions.service.WMSServiceException;
 
 import javax.validation.constraints.Min;
@@ -24,6 +25,34 @@ String key;
         minValidatorCondition.dateDeliver(object);
         conditions.add(1,minValidatorCondition);
     }
+    public void max(Object object){
+        MaxValidatorCondition maxValidatorCondition=new MaxValidatorCondition();
+        maxValidatorCondition.setKey(key);
+        maxValidatorCondition.dateDeliver(object);
+        conditions.add(1,maxValidatorCondition);
+    }
+    public void in(int[] object){
+        InValidatorCondition inValidatorCondition=new InValidatorCondition();
+        inValidatorCondition.setKey(key);
+        inValidatorCondition.dateDeliver(object);
+        conditions.add(1,inValidatorCondition);
+    }
+    public void length(Object object){
+        LengthValidatorCondition lengthValidatorCondition=new LengthValidatorCondition();
+        lengthValidatorCondition.setKey(key);
+        lengthValidatorCondition.dateDeliver(object);
+        conditions.add(1,lengthValidatorCondition);
+    }
+    public void notnull(){
+        NotnullValidatorCondition notnullValidatorCondition=new NotnullValidatorCondition();
+        notnullValidatorCondition.setKey(key);
+        conditions.add(1,notnullValidatorCondition);
+    }
+    public void notEmpty(){
+        NotEmptyValidatorCondition notEmptyValidatorCondition=new NotEmptyValidatorCondition();
+        notEmptyValidatorCondition.setKey(key);
+        conditions.add(1,notEmptyValidatorCondition);
+    }
 }
 class ValidatorCondition {
     public void validate(Object values) { }
@@ -47,6 +76,133 @@ class MinValidatorCondition extends ValidatorCondition {
         }
         if (actualValue < min) {
             throw new WMSServiceException(key+ "的值小于最低值");
+        }
+    }
+    public String getKey() {
+        return key;
+    }
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
+class MaxValidatorCondition extends ValidatorCondition {
+    private int max;
+    private int actualValue;
+    private String key;
+
+    public void dateDeliver(Object object) {
+        try {
+            max = Integer.parseInt(String.valueOf(object));
+        } catch (NumberFormatException e) {
+        }}
+
+    public void validate(Object value) {
+        try {
+            actualValue = Integer.parseInt(String.valueOf(value));
+
+        } catch (NumberFormatException e) {
+        }
+        if (actualValue > max) {
+            throw new WMSServiceException(key+ "的值大于最大值");
+        }
+    }
+    public String getKey() {
+        return key;
+    }
+    public void setKey(String key) {
+        this.key = key;
+    }
+}class InValidatorCondition extends ValidatorCondition {
+    private int min;
+    private int max;
+    private int actualValue;
+    private String key;
+
+    public void dateDeliver(int[] object) {
+        try {
+            min = Integer.parseInt(String.valueOf(object[0]));
+            max = Integer.parseInt(String.valueOf(object[1]));
+        } catch (NumberFormatException e) {
+        }}
+
+    public void validate(Object value) {
+        try {
+            actualValue = Integer.parseInt(String.valueOf(value));
+
+        } catch (NumberFormatException e) {
+        }
+        if (actualValue < min||actualValue>max) {
+            throw new WMSServiceException(key+ "的值不在指定的区间范围内");
+        }
+    }
+    public String getKey() {
+        return key;
+    }
+    public void setKey(String key) {
+        this.key = key;
+    }
+}class LengthValidatorCondition extends ValidatorCondition {
+    private int length;
+    private int actualValue;
+    private String key;
+
+    public void dateDeliver(Object object) {
+        try {
+            length = Integer.parseInt(String.valueOf(object));
+        } catch (NumberFormatException e) {
+        }}
+
+    public void validate(Object value) {
+        try {
+            actualValue = String.valueOf(value).length();
+
+        } catch (NumberFormatException e) {
+        }
+        if (actualValue > length) {
+            throw new WMSServiceException(key+ "字符串长度超过上限！");
+        }
+    }
+    public String getKey() {
+        return key;
+    }
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
+class NotnullValidatorCondition extends ValidatorCondition {
+    private int actualValue;
+    private String key;
+
+    public void validate(Object value) {
+        try {
+            actualValue = String.valueOf(value).length();
+
+        } catch (NumberFormatException e) {
+        }
+        if (actualValue == 0) {
+            throw new WMSServiceException(key+ "的值不能为空");
+        }
+    }
+    public String getKey() {
+        return key;
+    }
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
+class NotEmptyValidatorCondition extends ValidatorCondition {
+    private String actualValue;
+    private String key;
+
+
+    public void validate(Object value) {
+        try {
+            actualValue = String.valueOf(value);
+
+        } catch (NumberFormatException e) {
+        }
+        if (actualValue == "") {
+            throw new WMSServiceException(key+ "的值不能为空");
         }
     }
     public String getKey() {
--- WarehouseService/src/main/java/com/wms/services/warehouse/service/SupplierServicesImpl.java
+++ WarehouseService/src/main/java/com/wms/services/warehouse/service/SupplierServicesImpl.java
@@ -26,6 +26,10 @@ public class SupplierServicesImpl implements SupplierServices{
     {
 Validator validator=new Validator("ffaas");
 validator.min(100);
+int[] a={10 ,20};
+
+validator.in(a);
+        validator.notnull();
 validator.validate(20);
 
         for (int i=0;i<suppliers.length;i++) {
