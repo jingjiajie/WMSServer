20e0475172a818e601cdad50d0984129aec3ff4f
a098100 <a098100@163.com>
2018-05-01 14:25:30 +0800

    123

--- WarehouseService/src/main/java/com/wms/services/warehouse/dao/StorageAreaDAOImpl.java
+++ WarehouseService/src/main/java/com/wms/services/warehouse/dao/StorageAreaDAOImpl.java
@@ -1,10 +1,13 @@
 package com.wms.services.warehouse.dao;
 
+import com.wms.utilities.dao.BaseDAOImpl;
 import com.wms.utilities.model.StorageArea;
 import com.wms.utilities.datastructures.Condition;
 import com.wms.utilities.exceptions.dao.DatabaseNotFoundException;
 import com.wms.utilities.exceptions.dao.WMSDAOException;
 import com.wms.utilities.model.StorageAreaView;
+import com.wms.utilities.model.Warehouse;
+import com.wms.utilities.model.WarehouseView;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
@@ -15,27 +18,10 @@ import java.util.List;
 import java.util.stream.Stream;
 
 @Repository
-public class StorageAreaDAOImpl implements StorageAreaDAO {
-    @Autowired
-    SessionFactory sessionFactory;
-    private WarehouseServiceDAOTemplate<StorageArea, StorageAreaView> getDAOTemplate() {
-        return new WarehouseServiceDAOTemplate<>
-                (this.sessionFactory, "StorageArea", "StorageAreaView", StorageArea::getId);
-    }
-    @Override
-    public int[] add(String database, StorageArea[] storageAreas) throws WMSDAOException{
-        return this.getDAOTemplate().add(database, storageAreas);
-    }
-    @Override
-    public void update(String database,StorageArea storageAreas[]) throws WMSDAOException{
-        this.getDAOTemplate().update(database, storageAreas);
-    }
-    @Override
-    public void remove(String database, int ids[]) throws WMSDAOException{
-        this.getDAOTemplate().remove(database, ids);
-    }
-    @Override
-    public StorageAreaView[] find(String database,Condition cond) throws WMSDAOException{
-        return this.getDAOTemplate().find(database, cond, StorageAreaView.class);
+public class StorageAreaDAOImpl
+        extends BaseDAOImpl<StorageArea, StorageAreaView>
+        implements StorageAreaDAO{
+    public StorageAreaDAOImpl() {
+        super(StorageArea.class, StorageAreaView.class, StorageArea::getId);
     }
 }
