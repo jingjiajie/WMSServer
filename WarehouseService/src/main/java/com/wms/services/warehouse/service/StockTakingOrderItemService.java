a6935c4054a49bc88a1205b49c6a130169407e70
hsjjaaaa <a098100@163.com>
2018-05-07 22:24:02 +0800

    add stockTakingOrderAdd method

--- WarehouseService/src/main/java/com/wms/services/warehouse/service/StockTakingOrderItemService.java
+++ WarehouseService/src/main/java/com/wms/services/warehouse/service/StockTakingOrderItemService.java
@@ -1,9 +1,11 @@
 package com.wms.services.warehouse.service;
 
+import com.wms.services.warehouse.datastructures.StockTakingOrderItemAdd;
 import com.wms.utilities.model.StockTakingOrderItem;
 import com.wms.utilities.model.StockTakingOrderItemView;
 import com.wms.utilities.service.BaseService;
 
 public interface StockTakingOrderItemService
         extends BaseService<StockTakingOrderItem, StockTakingOrderItemView> {
+    public void addStockTakingOrderItemSingle(String accountBook, StockTakingOrderItemAdd stockTakingOrderItemAdd);
 }
--- WarehouseService/src/main/java/com/wms/services/warehouse/service/StockTakingOrderItemServiceImpl.java
+++ WarehouseService/src/main/java/com/wms/services/warehouse/service/StockTakingOrderItemServiceImpl.java
@@ -1,34 +1,60 @@
 package com.wms.services.warehouse.service;
 
 import com.wms.services.warehouse.dao.StockTakingOrderItemDAO;
+import com.wms.utilities.IDChecker;
 import com.wms.utilities.datastructures.Condition;
 import com.wms.utilities.exceptions.service.WMSServiceException;
 import com.wms.utilities.model.StockRecord;
 import com.wms.utilities.model.StockRecordView;
 import com.wms.utilities.model.StockTakingOrderItem;
 import com.wms.utilities.model.StockTakingOrderItemView;
+import com.wms.utilities.vaildator.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import com.wms.services.warehouse.datastructures.StockTakingOrderItemAdd;
 @Service
 @Transactional
 public class StockTakingOrderItemServiceImpl implements StockTakingOrderItemService{
 
     @Autowired
     StockTakingOrderItemDAO stockTakingOrderItemDAO;
+    @Autowired
+    StockTakingOrderService stockTakingOrderService;
+    @Autowired
+    IDChecker idChecker;
+    @Autowired
+    StockRecordService stockRecordService;
     @Override
     public int[] add(String accountBook, StockTakingOrderItem[] stockTakingOrderItems) throws WMSServiceException {
 
-
+        for (int i=0;i<stockTakingOrderItems.length;i++) {
+            new Validator("单位").notnull().notEmpty().validate(stockTakingOrderItems[i].getUnit());
+            new Validator("单位数量").notnull().validate(stockTakingOrderItems[i].getUnitAmount());
+        }
+        for (int i=0;i<stockTakingOrderItems.length;i++) {
+          idChecker.check(StockTakingOrderService.class,accountBook,stockTakingOrderItems[i].getStockTakingOrderId(),"盘点单");
+          idChecker.check(PersonService.class,accountBook,stockTakingOrderItems[i].getPersonId(),"盘点人");
+          idChecker.check(SupplyService.class,accountBook,stockTakingOrderItems[i].getSupplyId(),"供货信息");
+          idChecker.check(StorageLocationService.class,accountBook,stockTakingOrderItems[i].getStorageLocationId(),"库位");
+          }
         return stockTakingOrderItemDAO.add(accountBook,stockTakingOrderItems);
     }
 
     @Override
     public  void update(String accountBook,StockTakingOrderItem[] stockTakingOrderItems) throws WMSServiceException {
-
+        for (int i=0;i<stockTakingOrderItems.length;i++) {
+            new Validator("单位").notnull().notEmpty().validate(stockTakingOrderItems[i].getUnit());
+            new Validator("单位数量").notnull().validate(stockTakingOrderItems[i].getUnitAmount());
+        }
+        for (int i=0;i<stockTakingOrderItems.length;i++) {
+            idChecker.check(StockTakingOrderService.class,accountBook,stockTakingOrderItems[i].getStockTakingOrderId(),"盘点单");
+            idChecker.check(PersonService.class,accountBook,stockTakingOrderItems[i].getPersonId(),"盘点人");
+            idChecker.check(SupplyService.class,accountBook,stockTakingOrderItems[i].getSupplyId(),"供货信息");
+            idChecker.check(StorageLocationService.class,accountBook,stockTakingOrderItems[i].getStorageLocationId(),"库位");
+        }
+        stockTakingOrderItemDAO.update(accountBook,stockTakingOrderItems);
     }
-
 
     @Override
     public void remove(String accountBook, int[] ids) throws WMSServiceException{
@@ -40,13 +66,35 @@ public class StockTakingOrderItemServiceImpl implements StockTakingOrderItemServ
         }
     }
 
-
     @Override
     public StockTakingOrderItemView[] find(String accountBook, Condition cond) throws WMSServiceException{
         return this.stockTakingOrderItemDAO.find(accountBook, cond);
     }
 
+    @Override
+    public void addStockTakingOrderItemSingle(String accountBook, StockTakingOrderItemAdd stockTakingOrderItemAdd)
+    {
+        int mode=stockTakingOrderItemAdd.getMode();
 
+        //TODO
+        StockRecordView[] stockRecord=stockRecordService.find(accountBook,new Condition().addCondition("supplyId",new Integer[]{stockTakingOrderItemAdd.getSupplyId()}).
+                addCondition("warehouseId",new Integer[]{stockTakingOrderItemAdd.getWarehouseId()}));
+
+        if(stockRecord.length==0)
+        {
+            throw new WMSServiceException("没有查到此供货的任何库存信息");
+        }
+        //添加所有信息条目
+        if (mode==0){
+
+
+        }
+        //只添加总数量条目
+        if(mode==1){
+
+
+        }
+    }
 
 
 
