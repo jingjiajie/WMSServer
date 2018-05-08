47323f7b9e00e2e975941ee0b8fdc4e400053016
陈光晃 <wingmonth@outlook.com>
2018-05-08 20:11:01 +0800

    some

--- WarehouseService/src/main/java/com/wms/services/warehouse/service/DeliveryOrderService.java
+++ WarehouseService/src/main/java/com/wms/services/warehouse/service/DeliveryOrderService.java
@@ -1,10 +1,13 @@
 package com.wms.services.warehouse.service;
 
+import com.wms.services.warehouse.datastructures.TransferArgs;
 import com.wms.utilities.service.BaseService;
 import com.wms.utilities.model.DeliveryOrder;
 import com.wms.utilities.model.DeliveryOrderView;
 
 public interface DeliveryOrderService
  extends BaseService<DeliveryOrder,DeliveryOrderView>{
+
+    public void transferPakage(String accountBook, TransferArgs transferArgs);
 
 }
