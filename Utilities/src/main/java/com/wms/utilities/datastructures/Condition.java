dddb65d570128a0c4120c490e58537ddec6bdb2b
jingjiajie <932166095@qq.com>
2018-05-24 16:02:44 +0800

    fixed Condition Bug

--- Utilities/src/main/java/com/wms/utilities/datastructures/Condition.java
+++ Utilities/src/main/java/com/wms/utilities/datastructures/Condition.java
@@ -9,7 +9,10 @@ import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -37,10 +40,155 @@ public class Condition {
         return this.toJson();
     }
 
+    @Deprecated
     public Query createQuery(String entityName, Session session) {
         return this.createQuery(entityName, session, false);
     }
 
+    public Query createQuery(Class targetClass, Session session) {
+        return this.createQuery(targetClass, session, false);
+    }
+
+    @SuppressWarnings("all")
+    public Query createQuery(Class targetClass, Session session, boolean returnCount) {
+        String className = targetClass.getName();
+        List<ConditionItem> conditionItems = this.conditions;
+        List<OrderItem> orderItems = this.orders;
+        StringBuffer hqlString = new StringBuffer();
+        if (returnCount) {
+            hqlString.append("select count(*) ");
+        }
+        hqlString.append("from " + className + " ");
+        Map<String, Object> queryParams = new HashMap<String, Object>();
+        int valueNum = 0;
+        if (conditionItems != null) {
+            if (conditionItems.size() != 0) {
+                hqlString.append("where 1=1 ");
+            }
+
+            for (int i = 0; i < conditionItems.size(); i++, valueNum++) {
+                ConditionItem cond = conditionItems.get(i);
+                String key = cond.getKey();
+                Object[] condValues = cond.getValues();
+                Field targetField = null;
+                Class fieldType = null;
+                try {
+                    targetField = targetClass.getDeclaredField(key);
+                    fieldType = targetField.getType();
+                    for (int j = 0; j < condValues.length; j++) {
+                        condValues[j] = this.changeType(condValues[j], fieldType);
+                    }
+                } catch (NoSuchFieldException ex) {
+                    throw new WMSServiceException(className + " 类型中不存在\"" + key + "\"字段！查询失败");
+                }
+                switch (cond.getRelation()) {
+                    case EQUAL:
+                        if (condValues.length != 1) {
+                            throw new ConditionException("EQUAL relation needs one value");
+                        }
+                        hqlString.append(String.format(" AND %s = :value%d", cond.getKey(), valueNum));
+                        queryParams.put("value" + i, condValues[0]);
+                        break;
+                    case NOT_EQUAL:
+                        if (condValues.length != 1) {
+                            throw new ConditionException("NOT_EQUAL relation needs one value");
+                        }
+                        hqlString.append(String.format(" AND %s <> :value%d", cond.getKey(), valueNum));
+                        queryParams.put("value" + i, condValues[0]);
+                        break;
+                    case CONTAINS:
+                        if (condValues.length != 1) {
+                            throw new ConditionException("CONTAINS relation needs one value");
+                        }
+                        hqlString.append(String.format(" AND %s LIKE :value%d", cond.getKey(), valueNum));
+                        queryParams.put("value" + i, "%" + condValues[0] + "%");
+                        break;
+                    case GREATER_THAN:
+                        if (condValues.length != 1) {
+                            throw new ConditionException("GREATER_THAN relation needs one value");
+                        }
+                        hqlString.append(String.format(" AND %s > :value%d", cond.getKey(), valueNum));
+                        queryParams.put("value" + i, condValues[0]);
+                        break;
+                    case GREATER_THAN_OR_EQUAL_TO:
+                        if (condValues.length != 1) {
+                            throw new ConditionException("GREATER_THAN_OR_EQUAL_TO relation needs one value");
+                        }
+                        hqlString.append(String.format(" AND %s >= :value%d", cond.getKey(), valueNum));
+                        queryParams.put("value" + i, condValues[0]);
+                        break;
+                    case LESS_THAN:
+                        if (condValues.length != 1) {
+                            throw new ConditionException("LESS_THAN relation needs one value");
+                        }
+                        hqlString.append(String.format(" AND %s < :value%d", cond.getKey(), valueNum));
+                        queryParams.put("value" + i, condValues[0]);
+                        break;
+                    case LESS_THAN_OR_EQUAL_TO:
+                        if (condValues.length != 1) {
+                            throw new ConditionException("LESS_THAN_OR_EQUAL_TO relation needs one value");
+                        }
+                        hqlString.append(String.format(" AND %s <= :value%d", cond.getKey(), valueNum));
+                        queryParams.put("value" + i, condValues[0]);
+                        break;
+                    case BETWEEN:
+                        if (condValues.length != 2) {
+                            throw new ConditionException("BETWEEN relation needs two values");
+                        }
+                        hqlString.append(String.format(" AND %s BETWEEN :value%d_1 AND :value%d_2", cond.getKey(), valueNum, valueNum));
+                        queryParams.put("value" + i + "_1", condValues[0]);
+                        queryParams.put("value" + i + "_2", condValues[1]);
+                        break;
+                    default:
+                        throw new ConditionException("Unsupported relation " + cond.getRelation().toString());
+                }
+            }
+        }
+        //如果没有ID排序条件，则永远加一个按ID倒排。不需要理由
+        boolean hasIDOrder = false;
+        for (OrderItem orderItem : this.orders) {
+            if (orderItem.getKey().equalsIgnoreCase("id")) {
+                hasIDOrder = true;
+            }
+        }
+        if (!hasIDOrder) {
+            this.addOrder("id", OrderItem.Order.DESC);
+        }
+
+        if (orderItems != null) {
+            if (orderItems.size() > 0) {
+                hqlString.append(" order by ");
+            }
+            for (int i = 0; i < orderItems.size(); i++, valueNum++) {
+                if (i != 0) {
+                    hqlString.append(",");
+                }
+                hqlString.append(String.format("%s %s", orderItems.get(i).getKey(), orderItems.get(i).getOrder().toString()));
+            }
+        }
+        //创建查询
+        Query query = session.createQuery(hqlString.toString());
+
+        //如果设置了分页条件，则生成分页语句
+        if (this.page != -1) {
+            if (returnCount) {
+                throw new WMSServiceException(String.format("returnCount时不允许设置页码(%d)", page));
+            }
+            if (page == 0) {
+                throw new WMSServiceException(String.format("page(%d)必须从1开始！", page));
+            }
+            query.setFirstResult((page - 1) * pageSize);
+            query.setMaxResults(pageSize);
+        }
+
+        for (Map.Entry<String, Object> entry : queryParams.entrySet()) {
+            query.setParameter(entry.getKey(), entry.getValue());
+        }
+        return query;
+    }
+
+    @SuppressWarnings("all")
+    @Deprecated
     public Query createQuery(String entityName, Session session, boolean returnCount) {
         List<ConditionItem> conditionItems = this.conditions;
         List<OrderItem> orderItems = this.orders;
@@ -69,7 +217,7 @@ public class Condition {
                         } else if (doubleValue.longValue() == doubleValue) {
                             condValues[j] = doubleValue.longValue();
                         }
-                    }else if(condValues[j] instanceof String){
+                    } else if (condValues[j] instanceof String) {
                         //如果有日期字符串，转换为日期类型，避免SQL查询类型错误
                         SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
                         SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm");
@@ -156,12 +304,12 @@ public class Condition {
         }
         //如果没有ID排序条件，则永远加一个按ID倒排。不需要理由
         boolean hasIDOrder = false;
-        for(OrderItem orderItem : this.orders){
-            if(orderItem.getKey().equalsIgnoreCase("id")){
+        for (OrderItem orderItem : this.orders) {
+            if (orderItem.getKey().equalsIgnoreCase("id")) {
                 hasIDOrder = true;
             }
         }
-        if(!hasIDOrder) {
+        if (!hasIDOrder) {
             this.addOrder("id", OrderItem.Order.DESC);
         }
 
@@ -181,11 +329,11 @@ public class Condition {
 
         //如果设置了分页条件，则生成分页语句
         if (this.page != -1) {
-            if(returnCount){
-                throw new WMSServiceException(String.format("returnCount时不允许设置页码(%d)",page));
+            if (returnCount) {
+                throw new WMSServiceException(String.format("returnCount时不允许设置页码(%d)", page));
             }
-            if(page == 0){
-                throw new WMSServiceException(String.format("page(%d)必须从1开始！",page));
+            if (page == 0) {
+                throw new WMSServiceException(String.format("page(%d)必须从1开始！", page));
             }
             query.setFirstResult((page - 1) * pageSize);
             query.setMaxResults(pageSize);
@@ -257,5 +405,37 @@ public class Condition {
     public Condition addOrder(String key) {
         this.addOrder(key, OrderItem.Order.ASC);
         return this;
+    }
+
+    @SuppressWarnings("all")
+    private Object changeType(Object value, Class targetType) {
+        if (value == null) return null;
+        if (targetType == value.getClass()) return value;
+        try {
+            if (targetType == int.class) {
+                double doubleValue = Double.parseDouble(value.toString());
+                return (int) doubleValue;
+            } else if (targetType == double.class) {
+                return Double.parseDouble(value.toString());
+            } else if (targetType == boolean.class) {
+                return Boolean.parseBoolean(value.toString());
+            } else if (targetType == BigDecimal.class) {
+                return new BigDecimal(value.toString());
+            } else if (targetType == Timestamp.class) {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                sdf.setLenient(true);
+                try {
+                    return sdf.parse(value.toString());
+                } catch (Exception e) {
+                    throw new WMSServiceException("\"" + value.toString() + "\"不是合法的日期字符串！");
+                }
+            } else {
+                throw new WMSServiceException("未被支持的Condition类型：" + targetType.getName());
+            }
+        } catch (WMSServiceException e) {
+            throw e;
+        }catch (Exception e){
+            throw new WMSServiceException(String.format("\"%s\"不能转换为目标类型，请检查输入！"));
+        }
     }
 }
