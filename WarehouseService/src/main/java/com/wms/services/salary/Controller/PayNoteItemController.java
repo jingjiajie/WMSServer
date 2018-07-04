1c12bbe34c0e5ebacfc97943911c272ae6a43b8d
hsjjaaaa <a098100@163.com>
2018-07-04 21:18:55 +0800

    add payNote

---
+++ WarehouseService/src/main/java/com/wms/services/salary/Controller/PayNoteItemController.java
@@ -0,0 +1,8 @@
+package com.wms.services.salary.Controller;
+
+import com.wms.utilities.controller.BaseController;
+import com.wms.utilities.model.PayNoteItem;
+import com.wms.utilities.model.PayNoteItemView;
+
+public interface PayNoteItemController extends BaseController<PayNoteItem,PayNoteItemView> {
+}
---
+++ WarehouseService/src/main/java/com/wms/services/salary/dao/PayNoteItemDAO.java
@@ -0,0 +1,8 @@
+package com.wms.services.salary.dao;
+
+import com.wms.utilities.dao.BaseDAO;
+import com.wms.utilities.model.PayNoteItem;
+import com.wms.utilities.model.PayNoteItemView;
+
+public interface PayNoteItemDAO extends BaseDAO<PayNoteItem,PayNoteItemView>{
+}
