ff0362a2a28cfb750c20ee71f5501df4e81b5582
a098100 <a098100@163.com>
2018-07-07 16:15:52 +0800

    fix

--- WarehouseService/src/main/java/com/wms/services/salary/Service/PayNoteService.java
+++ WarehouseService/src/main/java/com/wms/services/salary/Service/PayNoteService.java
@@ -1,8 +1,11 @@
 package com.wms.services.salary.service;
 
+import com.wms.services.salary.datestructures.CalculateTax;
 import com.wms.utilities.model.PayNote;
 import com.wms.utilities.model.PayNoteView;
 import com.wms.utilities.service.BaseService;
 
 public interface PayNoteService extends BaseService<PayNote,PayNoteView> {
+    void confirmToAccountTitle(String accountBook, int payNoteId );
+    void realPay(String accountBook,int payNoteId);
 }
