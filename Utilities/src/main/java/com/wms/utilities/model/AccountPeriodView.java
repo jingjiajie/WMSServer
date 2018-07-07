9faa6c67a31de3de0e35f2404ef82b2401fbd0c2
a098100 <a098100@163.com>
2018-07-07 11:23:14 +0800

    update model

---
+++ Utilities/src/main/java/com/wms/utilities/model/AccountPeriodView.java
@@ -0,0 +1,148 @@
+package com.wms.utilities.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
+public class AccountPeriodView {
+    private int id;
+    private Integer warehouseId;
+    private Integer lastAccountPeriodId;
+    private String name;
+    private Timestamp startTime;
+    private Integer ended;
+    private Timestamp endTime;
+    private String warehouseName;
+    private String lastAccountPeriodName;
+
+    @Basic
+    @Id
+    @Column(name = "ID")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "WarehouseID")
+    public Integer getWarehouseId() {
+        return warehouseId;
+    }
+
+    public void setWarehouseId(Integer warehouseId) {
+        this.warehouseId = warehouseId;
+    }
+
+    @Basic
+    @Column(name = "LastAccountPeriodID")
+    public Integer getLastAccountPeriodId() {
+        return lastAccountPeriodId;
+    }
+
+    public void setLastAccountPeriodId(Integer lastAccountPeriodId) {
+        this.lastAccountPeriodId = lastAccountPeriodId;
+    }
+
+    @Basic
+    @Column(name = "Name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "StartTime")
+    public Timestamp getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Timestamp startTime) {
+        this.startTime = startTime;
+    }
+
+    @Basic
+    @Column(name = "Ended")
+    public Integer getEnded() {
+        return ended;
+    }
+
+    public void setEnded(Integer ended) {
+        this.ended = ended;
+    }
+
+    @Basic
+    @Column(name = "EndTime")
+    public Timestamp getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Timestamp endTime) {
+        this.endTime = endTime;
+    }
+
+    @Basic
+    @Column(name = "WarehouseName")
+    public String getWarehouseName() {
+        return warehouseName;
+    }
+
+    public void setWarehouseName(String warehouseName) {
+        this.warehouseName = warehouseName;
+    }
+
+    @Basic
+    @Column(name = "LastAccountPeriodName")
+    public String getLastAccountPeriodName() {
+        return lastAccountPeriodName;
+    }
+
+    public void setLastAccountPeriodName(String lastAccountPeriodName) {
+        this.lastAccountPeriodName = lastAccountPeriodName;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+
+        AccountPeriodView that = (AccountPeriodView) object;
+
+        if (id != that.id) return false;
+        if (warehouseId != null ? !warehouseId.equals(that.warehouseId) : that.warehouseId != null) return false;
+        if (lastAccountPeriodId != null ? !lastAccountPeriodId.equals(that.lastAccountPeriodId) : that.lastAccountPeriodId != null)
+            return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (startTime != null ? !startTime.equals(that.startTime) : that.startTime != null) return false;
+        if (ended != null ? !ended.equals(that.ended) : that.ended != null) return false;
+        if (endTime != null ? !endTime.equals(that.endTime) : that.endTime != null) return false;
+        if (warehouseName != null ? !warehouseName.equals(that.warehouseName) : that.warehouseName != null)
+            return false;
+        if (lastAccountPeriodName != null ? !lastAccountPeriodName.equals(that.lastAccountPeriodName) : that.lastAccountPeriodName != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (warehouseId != null ? warehouseId.hashCode() : 0);
+        result = 31 * result + (lastAccountPeriodId != null ? lastAccountPeriodId.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (startTime != null ? startTime.hashCode() : 0);
+        result = 31 * result + (ended != null ? ended.hashCode() : 0);
+        result = 31 * result + (endTime != null ? endTime.hashCode() : 0);
+        result = 31 * result + (warehouseName != null ? warehouseName.hashCode() : 0);
+        result = 31 * result + (lastAccountPeriodName != null ? lastAccountPeriodName.hashCode() : 0);
+        return result;
+    }
+}
