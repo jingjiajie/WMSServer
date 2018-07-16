96e21ac3b64b7f796d0b0fa04eb746a6880a7d1d
hsjjaaaa <a098100@163.com>
2018-07-16 20:48:43 +0800

    add accountSynchronize

--- WarehouseService/src/main/java/com/wms/services/salary/service/PayNoteService.java
+++ WarehouseService/src/main/java/com/wms/services/salary/service/PayNoteService.java
@@ -1,5 +1,6 @@
 package com.wms.services.salary.service;
 
+import com.wms.services.salary.datestructures.AccountSynchronize;
 import com.wms.services.salary.datestructures.PayNoteAndItems;
 import com.wms.utilities.exceptions.service.WMSServiceException;
 import com.wms.utilities.model.PayNote;
@@ -9,7 +10,7 @@ import com.wms.utilities.service.BaseService;
 import java.util.List;
 
 public interface PayNoteService extends BaseService<PayNote,PayNoteView> {
-    void confirmToAccountTitle(String accountBook, int payNoteId,int personId );
-    void realPayToAccountTitle(String accountBook,int payNoteId,int personId);
+    void confirmToAccountTitle(String accountBook, AccountSynchronize accountSynchronize);
+    void realPayToAccountTitle(String accountBook,AccountSynchronize accountSynchronize);
     List<PayNoteAndItems> getPreviewData(String accountBook, List<Integer> payNoteIds) throws WMSServiceException;
 }
