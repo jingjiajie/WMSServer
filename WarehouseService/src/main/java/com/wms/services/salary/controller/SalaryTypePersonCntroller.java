b065a1474b84e6b6bde37cbb24a7db3fa1ec5f72
a098100 <a098100@163.com>
2018-07-20 15:55:15 +0800

    add salaeryTypePerson

---
+++ WarehouseService/src/main/java/com/wms/services/salary/controller/SalaryTypePersonCntroller.java
@@ -0,0 +1,9 @@
+package com.wms.services.salary.controller;
+
+import com.wms.utilities.controller.BaseController;
+import com.wms.utilities.model.SalaryType;
+import com.wms.utilities.model.SalaryTypePerson;
+import com.wms.utilities.model.SalaryTypePersonView;
+
+public interface SalaryTypePersonCntroller extends BaseController<SalaryTypePerson,SalaryTypePersonView> {
+}
