5b43305d470b736c1f8c2053261cd1bca41c4cdf
a098100 <a098100@163.com>
2018-07-22 20:43:59 +0800

    add personSalary

--- WarehouseService/src/main/java/com/wms/services/salary/controller/PersonSalaryController.java
+++ WarehouseService/src/main/java/com/wms/services/salary/controller/PersonSalaryController.java
@@ -1,8 +1,11 @@
 package com.wms.services.salary.controller;
 
+import com.wms.services.salary.datestructures.AddAllItem;
+import com.wms.services.salary.datestructures.AddPersonSalary;
 import com.wms.utilities.controller.BaseController;
 import com.wms.utilities.model.PersonSalary;
 import com.wms.utilities.model.PersonSalaryView;
 
 public interface PersonSalaryController extends BaseController<PersonSalary,PersonSalaryView> {
+    void addPersonSalary(String accountBook, AddPersonSalary addPersonSalary);
 }
