8d70f4ef2263216f82d0083ff4625d9dd58117e2
a098100 <a098100@163.com>
2018-08-26 10:26:24 +0800

    fix

--- WarehouseService/src/main/java/com/wms/services/salary/datestructures/SalaryItemTypeState.java
+++ WarehouseService/src/main/java/com/wms/services/salary/datestructures/SalaryItemTypeState.java
@@ -4,4 +4,7 @@ public class SalaryItemTypeState {
     public static final int VALUATION_SALARY=1;
     public static final int  REGULAR_SALARY=0;
     public static final int  Formula=2;
+
+    public static final int GIVE_OUT_OFF = 0;
+    public static final int GIVE_OUT_ON=1;
 }
