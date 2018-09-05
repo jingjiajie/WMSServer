8624319cd8e99864f1bdda8aa51f67eea91a1f81
hsjjaaaa <a098100@163.com>
2018-09-05 15:34:51 +0800

    summary note dao

---
+++ WarehouseService/src/main/java/com/wms/services/settlemen/dao/SummaryNoteDAOImpl.java
@@ -0,0 +1,14 @@
+package com.wms.services.settlemen.dao;
+
+import com.wms.utilities.dao.BaseDAOImpl;
+import com.wms.utilities.model.SummaryNote;
+import com.wms.utilities.model.SummaryNoteView;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SummaryNoteDAOImpl
+        extends BaseDAOImpl<SummaryNote,SummaryNoteView>
+        implements SummaryNoteDAO {
+    public SummaryNoteDAOImpl(){
+        super(SummaryNote.class,SummaryNoteView.class,SummaryNote::getId);}
+}
