1cddcb2480f2a94056bc27894122256fb0742fb2
陈光晃 <wingmonth@outlook.com>
2018-09-06 11:24:31 +0800

    invoice

---
+++ WarehouseService/src/main/java/com/wms/services/settlement/controller/InvoiceController.java
@@ -0,0 +1,8 @@
+package com.wms.services.settlement.controller;
+
+import com.wms.utilities.controller.BaseController;
+import com.wms.utilities.model.Invoice;
+import com.wms.utilities.model.InvoiceView;
+
+public interface InvoiceController extends BaseController<Invoice,InvoiceView> {
+}
