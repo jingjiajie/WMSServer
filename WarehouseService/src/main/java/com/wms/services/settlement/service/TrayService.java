7538d91a82d06f44f4b96913ec122863a9099663
a098100 <a098100@163.com>
2018-09-13 15:39:32 +0800

    tray

---
+++ WarehouseService/src/main/java/com/wms/services/settlement/service/TrayService.java
@@ -0,0 +1,7 @@
+package com.wms.services.settlement.service;
+
+import com.wms.utilities.model.CommonData;
+import com.wms.utilities.service.BaseService;
+
+public interface TrayService extends BaseService<CommonData,CommonData> {
+}
