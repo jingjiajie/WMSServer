7f501834fd001b612130ad776bb30af1e4a6cc2e
hsjjaaaa <a098100@163.com>
2018-09-13 09:12:40 +0800

    fix

--- WarehouseService/src/main/java/com/wms/services/settlement/service/TrayThresholdsServiceImpl.java
+++ WarehouseService/src/main/java/com/wms/services/settlement/service/TrayThresholdsServiceImpl.java
@@ -9,11 +9,15 @@ import com.wms.utilities.model.TrayThresholds;
 import com.wms.utilities.model.TrayThresholdsView;
 import com.wms.utilities.vaildator.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
+@Service
+@Transactional
 public class TrayThresholdsServiceImpl implements TrayThresholdsService{
     @Autowired
     TrayThresholdsDAO trayThresholdsDAO;
