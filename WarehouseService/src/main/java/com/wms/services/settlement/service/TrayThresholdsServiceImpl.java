d8daed267921a4062c5e71ddac10bc12f524901f
a098100 <a098100@163.com>
2018-09-23 17:18:20 +0800

    fix

--- WarehouseService/src/main/java/com/wms/services/settlement/service/TrayThresholdsServiceImpl.java
+++ WarehouseService/src/main/java/com/wms/services/settlement/service/TrayThresholdsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.wms.services.settlement.service;
 
 import com.wms.services.settlement.dao.TrayThresholdsDAO;
+import com.wms.services.warehouse.service.WarehouseService;
 import com.wms.utilities.datastructures.Condition;
 import com.wms.utilities.exceptions.service.WMSServiceException;
 import com.wms.utilities.model.SummaryNote;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -21,22 +23,24 @@ import java.util.stream.Stream;
 public class TrayThresholdsServiceImpl implements TrayThresholdsService{
     @Autowired
     TrayThresholdsDAO trayThresholdsDAO;
+    @Autowired
+    WarehouseService warehouseService;
     @Override
     public int[] add(String accountBook, TrayThresholds[] trayThresholds) throws WMSServiceException
     {
 
-        //his.validateEntities(accountBook,trayThresholds);
+        this.validateEntities(accountBook,trayThresholds);
         int[] ids= trayThresholdsDAO.add(accountBook,trayThresholds);
-        //this.validateDuplication(accountBook,summaryNotes);
+        this.validateDuplication(accountBook,trayThresholds);
         return ids;
     }
 
     @Override
     public void update(String accountBook, TrayThresholds[] trayThresholds) throws WMSServiceException
     {
-        //this.validateEntities(accountBook,summaryNotes);
+        this.validateEntities(accountBook,trayThresholds);
         trayThresholdsDAO.update(accountBook, trayThresholds);
-        //this.validateDuplication(accountBook,summaryNotes);
+        this.validateDuplication(accountBook,trayThresholds);
     }
 
     @Override
@@ -59,41 +63,34 @@ public class TrayThresholdsServiceImpl implements TrayThresholdsService{
     public TrayThresholdsView[] find(String accountBook, Condition cond) throws WMSServiceException {
         return this.trayThresholdsDAO.find(accountBook, cond);
     }
-/*
-    private void validateEntities(String accountBook,SummaryNote[] summaryNotes) throws WMSServiceException{
-        Stream.of(summaryNotes).forEach((summaryNote -> {
-            new Validator("代号").notEmpty().validate(summaryNote.getNo());
-            new Validator("起始时间").notEmpty().validate(summaryNote.getStartTime());
-            new Validator("截止时间").notEmpty().validate(summaryNote.getEndTime());
-            if(summaryNote.getStartTime().compareTo(summaryNote.getEndTime())>=0)
-            {
-                throw new WMSServiceException("汇总单的截止时间必须在起始时间之后！单号："+summaryNote.getNo());
+
+    private void validateEntities(String accountBook,TrayThresholds[] trayThreshold) throws WMSServiceException{
+        Stream.of(trayThreshold).forEach((summaryNote -> {
+            new Validator("阙值").notEmpty().min(0).validate(summaryNote.getThreshold());
+            if(summaryNote.getThreshold().compareTo(new BigDecimal(1))>0){
+                throw new WMSServiceException("阙值不能大于1");
             }
             if(this.warehouseService.find(accountBook,
                     new Condition().addCondition("id",summaryNote.getWarehouseId())).length == 0){
                 throw new WMSServiceException(String.format("仓库不存在，请重新提交！(%d)",summaryNote.getWarehouseId()));
             }
-            if(this.personService.find(accountBook,
-                    new Condition().addCondition("id",summaryNote.getCreatePersonId())).length == 0){
-                throw new WMSServiceException(String.format("人员不存在，请重新提交！(%d)",summaryNote.getCreatePersonId()));
-            }
         }));
     }
 
-    private void validateDuplication(String accountBook,SummaryNote[] summaryNotes)
+    private void validateDuplication(String accountBook,TrayThresholds[] trayThresholds)
     {
         Condition cond = new Condition();
-        cond.addCondition("warehouseId",summaryNotes[0].getWarehouseId());
-        SummaryNote[] summaryNotesCheck=summaryNoteDAO.findTable(accountBook,cond);
-        List<SummaryNote> summaryNoteList= Arrays.asList(summaryNotesCheck);
-        summaryNoteList.stream().reduce((last, cur) -> {
-            if (last.getNo().equals(cur.getNo())){
-                throw new WMSServiceException("汇总单单号重复:"+cur.getNo());
+        cond.addCondition("warehouseId",trayThresholds[0].getWarehouseId());
+        TrayThresholds[] trayThresholds1=trayThresholdsDAO.findTable(accountBook,cond);
+        List<TrayThresholds> trayThresholds2= Arrays.asList(trayThresholds1);
+        trayThresholds2.stream().reduce((last, cur) -> {
+            if (last.getThreshold().equals(cur.getThreshold())){
+                throw new WMSServiceException("阙值不能重复:"+cur.getThreshold());
             }
             return cur;
         });
     }
-*/
+
     @Override
     public long findCount(String database,Condition cond) throws WMSServiceException{
         return this.trayThresholdsDAO.findCount(database,cond);
