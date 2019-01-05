2a52d404e533acd66f6f4d987e18238b1bfd10d8
hsjjaaaa <a098100@163.com>
2019-01-05 15:53:45 +0800

    update model

--- WarehouseService/src/main/java/com/wms/services/salary/dao/PersonSalaryDAOImpl.java
+++ WarehouseService/src/main/java/com/wms/services/salary/dao/PersonSalaryDAOImpl.java
@@ -2,12 +2,13 @@ package com.wms.services.salary.dao;
 
 import com.wms.utilities.dao.BaseDAOImpl;
 import com.wms.utilities.datastructures.Condition;
+import com.wms.utilities.datastructures.ConditionItem;
+import com.wms.utilities.datastructures.OrderItem;
+import com.wms.utilities.exceptions.ConditionException;
 import com.wms.utilities.exceptions.dao.DatabaseNotFoundException;
 import com.wms.utilities.exceptions.dao.WMSDAOException;
-import com.wms.utilities.model.PersonSalary;
-import com.wms.utilities.model.PersonSalaryView;
-import com.wms.utilities.model.PersonSalaryWithSumAmount;
-import com.wms.utilities.model.StockRecordViewNewest;
+import com.wms.utilities.exceptions.service.WMSServiceException;
+import com.wms.utilities.model.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
@@ -15,32 +16,62 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
-public class PersonSalaryDAOImpl extends BaseDAOImpl<PersonSalary,PersonSalaryView> implements PersonSalaryDAO {
-    public PersonSalaryDAOImpl(){
-        super(PersonSalary.class,PersonSalaryView.class,PersonSalary::getId);}
+public class PersonSalaryDAOImpl extends BaseDAOImpl<PersonSalary, PersonSalaryView> implements PersonSalaryDAO {
+    public PersonSalaryDAOImpl() {
+        super(PersonSalary.class, PersonSalaryView.class, PersonSalary::getId);
+    }
 
     @Autowired
     private SessionFactory sessionFactory;
+
     @Override
-    public PersonSalaryWithSumAmount[] findSum(String accountBook, Condition condition){
-        Session session = sessionFactory.getCurrentSession();
+    public PersonSalaryWithSumAmount[] findSum(String accountBook, Condition condition) {
+//        Session session = sessionFactory.getCurrentSession();
+//        try {
+//            session.createNativeQuery("USE " + accountBook + ";").executeUpdate();
+//        } catch (Throwable ex) {
+//            throw new DatabaseNotFoundException(accountBook);
+//        }
+//        try {
+//            Query query = condition.createQuery(PersonSalaryWithSumAmount.class, session);
+//            List<PersonSalaryWithSumAmount> resultList = query.list();
+//            PersonSalaryWithSumAmount[] resultArray = (PersonSalaryWithSumAmount[]) Array.newInstance(PersonSalaryWithSumAmount.class,resultList.size());
+//            resultList.toArray(resultArray);
+//            return resultArray;
+//        } catch (Throwable ex) {
+//            throw new WMSDAOException(ex.getMessage());
+//        }
+
+        Session session = this.sessionFactory.getCurrentSession();
+        session.flush();
         try {
             session.createNativeQuery("USE " + accountBook + ";").executeUpdate();
         } catch (Throwable ex) {
             throw new DatabaseNotFoundException(accountBook);
         }
-        try {
-            Query query = condition.createQuery(PersonSalaryWithSumAmount.class, session);
-            List<PersonSalaryWithSumAmount> resultList = query.list();
-            PersonSalaryWithSumAmount[] resultArray = (PersonSalaryWithSumAmount[]) Array.newInstance(PersonSalaryWithSumAmount.class,resultList.size());
-            resultList.toArray(resultArray);
-            return resultArray;
-        } catch (Throwable ex) {
-            throw new WMSDAOException(ex.getMessage());
-        }
+        Query querySum = null;
+        Query queryAll = null;
+        String sqlNew = "select * from \n" +
+                "(select id,WarehouseID,SalaryPeriodID,PersonID,SalaryItemID,sum(amount) as \"amount\",WarehouseName,PersonName,SalaryPeriodName,\"总金额\" as SalaryItemName\n" +
+                ",Edited,SalaryTypeID,GiveOut,post,salaryTypeName from PersonSalaryView where GiveOut=1\n" +
+                "GROUP BY PersonID,SalaryTypeID,WarehouseID,SalaryPeriodID) as PWS";
+        String sqlPostfix = " where 1=1";
+        sqlPostfix = this.addConditions(condition, sqlPostfix);
+        session.flush();
+        querySum = session.createNativeQuery(sqlNew + sqlPostfix, PersonSalaryWithSumAmount.class);
+        queryAll = session.createNativeQuery("select * from PersonSalaryView as PWS " + sqlPostfix, PersonSalaryWithSumAmount.class);
+        List<PersonSalaryWithSumAmount> resultListSum = querySum.list();
+        List<PersonSalaryWithSumAmount> resultListAll = queryAll.list();
+        resultListSum.addAll(resultListAll);
+        PersonSalaryWithSumAmount[] resultArray = (PersonSalaryWithSumAmount[]) Array.newInstance(PersonSalaryWithSumAmount.class, resultListSum.size());
+        resultListSum.toArray(resultArray);
+        return resultArray;
     }
 
     public long findCountSum(String database, Condition cond) throws WMSDAOException {
@@ -51,11 +82,25 @@ public class PersonSalaryDAOImpl extends BaseDAOImpl<PersonSalary,PersonSalaryVi
             throw new DatabaseNotFoundException(database);
         }
         try {
-            Query query = cond.createQuery(PersonSalaryWithSumAmount.class, session,true);
-            long result = (long)query.list().get(0);
+            Query query = cond.createQuery(PersonSalaryWithSumAmount.class, session, true);
+            long result = (long) query.list().get(0);
             return result;
         } catch (Throwable ex) {
             throw new WMSDAOException(ex.getMessage());
         }
+    }
+
+    private String addConditions(Condition condition, String sqlPostfix) {
+        ConditionItem[] conditionItems = condition.getConditions();
+        for (int i = 0; i < conditionItems.length; i++) {
+            ConditionItem conditionItem = conditionItems[i];
+            if (conditionItem.getKey().equals("salaryPeriodId")) {
+                sqlPostfix = sqlPostfix + " and PWS.SalaryPeriodId=" + conditionItem.getValues()[0].toString();
+            }
+            if (conditionItem.getKey().equals("salaryTypeId")) {
+                sqlPostfix = sqlPostfix + " and PWS.SalaryTypeId=" + conditionItem.getValues()[0].toString();
+            }
+        }
+        return sqlPostfix;
     }
 }
