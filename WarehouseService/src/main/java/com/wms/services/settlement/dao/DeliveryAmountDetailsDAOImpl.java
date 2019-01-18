9fde4ebd01dd411a48d2fc214260567b02e380ef
hsjjaaaa <a098100@163.com>
2019-01-18 11:40:37 +0800

    fix

--- WarehouseService/src/main/java/com/wms/services/settlement/dao/DeliveryAmountDetailsDAOImpl.java
+++ WarehouseService/src/main/java/com/wms/services/settlement/dao/DeliveryAmountDetailsDAOImpl.java
@@ -3,7 +3,9 @@ package com.wms.services.settlement.dao;
 import com.wms.utilities.dao.BaseDAOImpl;
 import com.wms.utilities.model.DeliveryAmountDetails;
 import com.wms.utilities.model.DeliveryAmountDetailsView;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class DeliveryAmountDetailsDAOImpl
         extends BaseDAOImpl<DeliveryAmountDetails, DeliveryAmountDetailsView>
         implements DeliveryAmountDetailsDAO {
