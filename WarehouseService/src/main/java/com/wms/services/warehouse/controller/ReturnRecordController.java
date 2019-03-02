19036dcb4d75e3032c8c83efc46a03f63bf82bf9
hsjjaaaa <a098100@163.com>
2019-03-02 09:57:10 +0800

    fix

--- WarehouseService/src/main/java/com/wms/services/warehouse/controller/ReturnRecordController.java
+++ WarehouseService/src/main/java/com/wms/services/warehouse/controller/ReturnRecordController.java
@@ -1,6 +1,8 @@
 package com.wms.services.warehouse.controller;
 
 import com.wms.utilities.controller.BaseController;
+import com.wms.utilities.model.ReturnRecord;
+import com.wms.utilities.model.ReturnRecordView;
 
 public interface ReturnRecordController
         extends BaseController<ReturnRecord,ReturnRecordView> {
